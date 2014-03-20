5cc58dd163d9e74983c2963dec6b3798add78de4
bwithbala <balakrishnan.tharumar@gmail.com>
2014-03-20 17:20:20 -0400

    Commit

--- js/main.js
+++ js/main.js
@@ -1,62 +1,30 @@
 var app = {
 
-		showAlert: function (message, title) {
-		    if (navigator.notification) {
-		        navigator.notification.alert(message, null, title, 'OK');
-		    } else {
-		        alert(title ? (title + ": " + message) : message);
-		    }
-		},		
-		
-/*    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+
+    registerEvents: function() {
+        $('body').on('mousedown', 'a', function(event) {
+            $(event.target).addClass('tappable-active');
         });
-    },*/
-		
-		findByName: function() {
-		    var self = this;
-		    this.store.findByName($('.search-key').val(), function(employees) {
-		        $('.employee-list').html(self.employeeLiTpl(employees));
-		    });
-		},		
-    
-    renderHomeView: function() {
-    	
-        /*var html =
-                "<div class='header'><h1>Home</h1></div>" +
-                "<div class='search-view'>" +
-                "<input class='search-key'/>" +
-                "<ul class='employee-list'></ul>" +
-                "</div>"
-        $('body').html(html);*/
-    	$('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },    
-    
+        $('body').on('mouseup', 'a', function(event) {
+            $(event.target).removeClass('tappable-active');
+        });
+    },
 
     initialize: function() {
-    	
-   var self = this;
-	this.homeTpl = Handlebars.compile($("#home-tpl").html());
-	this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());    
+        var self = this;
+        this.registerEvents();
         this.store = new MemoryStore(function() {
-         //   self.showAlert('Store Initialized', 'Info');
-        	self.renderHomeView();
+            $('body').html(new HomeView(self.store).render().el);
         });
-//        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    	
     }
 
 };
 
 app.initialize();
-
-
