aa44c69075f943a00505e1ea8319e360df00fca3
bwithbala <balakrishnan.tharumar@gmail.com>
2014-03-20 17:04:36 -0400

    Commit with Using Handlebar Templates

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ var app = {
 		    }
 		},		
 		
-    findByName: function() {
+/*    findByName: function() {
         console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
             var l = employees.length;
@@ -19,27 +19,42 @@ var app = {
                 $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
             }
         });
-    },
+    },*/
+		
+		findByName: function() {
+		    var self = this;
+		    this.store.findByName($('.search-key').val(), function(employees) {
+		        $('.employee-list').html(self.employeeLiTpl(employees));
+		    });
+		},		
     
     renderHomeView: function() {
-        var html =
+    	
+        /*var html =
                 "<div class='header'><h1>Home</h1></div>" +
                 "<div class='search-view'>" +
                 "<input class='search-key'/>" +
                 "<ul class='employee-list'></ul>" +
                 "</div>"
-        $('body').html(html);
+        $('body').html(html);*/
+    	$('body').html(this.homeTpl());
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },    
     
 
     initialize: function() {
-        var self = this;
+    	
+     /*   var self = this;
         this.store = new MemoryStore(function() {
          //   self.showAlert('Store Initialized', 'Info');
         	self.renderHomeView();
         });
 //        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+        */
+    	
+    	this.homeTpl = Handlebars.compile($("#home-tpl").html());
+    	this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());    	
+    	
     }
 
 };
